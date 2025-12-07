ce467ff4d3cf5e503a651f63acd6c79bb4710e5f
dubeycode <dubeysatyam3635@gmal.com>
2025-12-07 12:00:27 +0530

    location acess or error defind

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,12 +1,80 @@
+import WeatherSkeleton from "@/components/loading-skeleton";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
-import { RefreshCw } from "lucide-react";
+import { useGeolocation } from "@/hooks/use-geolocation";
+import { MapPin, RefreshCw } from "lucide-react";
 
 const WeatherDashboard = () => {
     
+const {coordinates
+  ,error:locationError,
+  getLocation,
+  isLoading:locationLoading,
+
+} =useGeolocation();
+
+console.log(coordinates);
+
+const handleRefresh = ()=>{
+  getLocation();
+  if(coordinates){
+    // reload weather data
+  }
+};
+
+ if(locationLoading){
+  return <WeatherSkeleton/>
+ }
+
+ if(locationError){
+  return(
+  <Alert variant="destructive">
+    <AlertTitle className="h-4 w-4" />
+    <AlertTitle>Location Error</AlertTitle>
+    <AlertDescription className="flex flex-col gap-4">
+      <p>{locationError}</p>
+      <Button onClick={getLocation} variant={"outline"} className="w-fit">
+        <MapPin className="mr-2 h-4 w-4" />
+        Enable Location
+      </Button>
+    </AlertDescription>
+  </Alert>
+  );
+ }
+
+ if(!coordinates){
+  return(
+  <Alert variant="destructive">
+    <AlertTitle>Location Required</AlertTitle>
+    <AlertDescription className="flex flex-col gap-4">
+      <p>Plese enable location acess to see your location weather </p>
+      <Button onClick={getLocation} variant={"outline"} className="w-fit">
+        <MapPin className="mr-2 h-4 w-4" />
+        Enable Location
+      </Button>
+    </AlertDescription>
+  </Alert>
+  );
+ }
+
 
 
   return (
-   
+    <div className="space-y-4">
+      {/* Favorite Cities */}
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-bold tracking-tight">My Location</h1>
+        <Button
+          variant={"outline"}
+          size={"icon"}
+          onClick={handleRefresh}
+          // disabled={}
+        >
+          <RefreshCw className="h-4 w-4" />
+        </Button>
+      </div>
+      {/* Current and Hourly weatehr */}
+    </div>
   );
 };
 
